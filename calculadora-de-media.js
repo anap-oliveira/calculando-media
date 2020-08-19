400068fb75f3f6b7c05ed813949c8d2398dfaf4f
anap-oliveira <paula.apx@gmail.com>
2020-08-19 13:36:08 -0300

    Calculadora de media finalizada

--- calculadora-de-media.js
+++ calculadora-de-media.js
@@ -9,16 +9,16 @@ const nota4 = input.question('Informe nota 4 ')
 const nota5 = input.question('Informe nota 5 ')
 const nota6 = input.question('Informe nota 6 ')
 
-const media = parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4) + parseFloat(nota5) + parseFloat(nota6) / 6
+const total = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4) + parseFloat(nota5) + parseFloat(nota6)) / 6
 
-console.log(media.toFixed(2))
+console.log(total.toFixed(2))
 
-if (media >= 7){
-    console.log("Aprovada")
-    } else if (media < 5){
-        console.log("Reprovado")
+if (total >= 7){
+    console.log("Parabéns! Você é desenrolada e está aprovada!")
+    } else if (total < 5){
+        console.log("Eita, mana! Deu ruim, você foi reprovada.")
     } else {
-        console.log("Recuperacao ")
+        console.log("Olhaí, ainda resta uma chance! Você está em recuperacao ")
         }
 
 
